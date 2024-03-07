aa01016a52ead1f97427bbe93c717f3fc58ea8cf
Luis Schroeder <t-luisg@microsoft.com>
2024-03-07 16:29:16 +0100

    Implemented result partition by filepath in order to improve output format

--- arm-ttk/Format-Json.ps1
+++ arm-ttk/Format-Json.ps1
@@ -1,10 +1,13 @@
 Function Format-Json {
     <#
     .SYNOPSIS
-        Takes results from ARMTTK and exports them as a JSON blob.
+        Takes results from ARMTTK and exports them as a JSON blob, grouped by file path.
+
     .DESCRIPTION
         Takes results from ARMTTK and exports them as JSON. The test cases include the filename, name of the test,
-        whether the test was successful, and help text for how to resolve the error if the test failed.
+        whether the test was successful, and help text for how to resolve the error if the test failed. Test results are 
+        grouped by their respective file paths.
+
     #>
     [CmdletBinding()]
     Param (
@@ -14,8 +17,8 @@ Function Format-Json {
     )
 
     Begin {
-        # Initialize the array to collect processed test cases
-        $TestCases = @()
+        # Initialize the hashtable to collect processed test cases grouped by file paths
+        $TestCasesGrouped = @{}
     }
 
     Process {
@@ -29,11 +32,11 @@ Function Format-Json {
         if ($TestResult.Passed) {
             $TestCase.optional = $false
         }
-        elseif ($null -ne $($TestResult.Warnings)) {
+        elseif ($null -ne $TestResult.Warnings) {
             $TestCase.optional = $true
             $TestCase.message = "$($TestResult.Warnings.Message.Replace('"', '\"')) in template file $($TestResult.file.name)"
         }
-        elseif ($null -ne $($TestResult.Errors)) {
+        elseif ($null -ne $TestResult.Errors) {
             $TestCase.optional = $false
             $TestCase.message = "$($TestResult.Errors.Exception.Message.Replace('"', '\"')) in template file $($TestResult.file.name)"
         }
@@ -42,12 +45,23 @@ Function Format-Json {
             $TestCase.message = "Unknown error in template file " + $TestResult.file.name
         }
 
-        $TestCases += $TestCase
+        # Check if the filepath key exists in the hashtable, if not create an array for it
+        if (-not $TestCasesGrouped.ContainsKey($TestCase.filepath)) {
+            $TestCasesGrouped[$TestCase.filepath] = @()
+        }
+
+        # Add the test case to the array for this filepath
+        $TestCasesGrouped[$TestCase.filepath] += $TestCase
     }
 
     End {
-        # Convert the array of hashtables to JSON
-        $JSON = $TestCases | ConvertTo-Json -Depth 5
+        # Iterate over each group in the hashtable and convert each group to JSON
+        $JSON = $TestCasesGrouped.GetEnumerator() | ForEach-Object {
+            @{ 
+                filepath = $_.Key
+                tests = $_.Value
+            }
+        } | ConvertTo-Json -Depth 5
 
         # Print the JSON string to the console
         Write-Output $JSON
